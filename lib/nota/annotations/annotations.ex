30c68173717efaa7a363240206ff999f65826200
Benjamin Leeds <baleeds@ncsu.edu>
2019-09-23 22:35:50 -0400

    Remove comments

--- lib/nota/annotations/annotations.ex
+++ lib/nota/annotations/annotations.ex
@@ -25,24 +25,8 @@ defmodule Nota.Annotations do
     queryable
   end
 
-  @doc """
-  Returns the list of annotation.
-
-  ## Examples
-
-      iex> list_annotation()
-      [%Annotation{}, ...]
-
-  """
   def list_annotations do
     Repo.all(Annotation)
-  end
-
-  def list_public_annotations(verse_id, user_id) do
-    Annotation
-    |> where([a], a.user_id != ^user_id)
-    |> where([a], a.verse_id == ^verse_id)
-    |> Repo.all
   end
 
   def list_annotations(%{user_id: user_id}) do
@@ -57,54 +41,23 @@ defmodule Nota.Annotations do
     |> Repo.all
   end
 
-  @doc """
-  Gets a single annotation.
+  def list_public_annotations(verse_id, user_id) do
+    Annotation
+    |> where([a], a.user_id != ^user_id)
+    |> where([a], a.verse_id == ^verse_id)
+    |> Repo.all
+  end
 
-  Raises `Ecto.NoResultsError` if the Annotation does not exist.
-
-  ## Examples
-
-      iex> get_annotation!(123)
-      %Annotation{}
-
-      iex> get_annotation!(456)
-      ** (Ecto.NoResultsError)
-
-  """
   def get_annotation!(id), do: Repo.get!(Annotation, id)
 
   def get_annotation(id), do: Repo.get(Annotation, id)
 
-  @doc """
-  Creates a annotation.
-
-  ## Examples
-
-      iex> create_annotation(%{field: value})
-      {:ok, %Annotation{}}
-
-      iex> create_annotation(%{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def create_annotation(attrs \\ %{}) do
     %Annotation{}
     |> Annotation.changeset(attrs)
     |> Repo.insert()
   end
 
-  @doc """
-  Updates a annotation.
-
-  ## Examples
-
-      iex> update_annotation(annotation, %{field: new_value})
-      {:ok, %Annotation{}}
-
-      iex> update_annotation(annotation, %{field: bad_value})
-      {:error, %Ecto.Changeset{}}
-
-  """
   def update_annotation(%Annotation{} = annotation, attrs) do
     annotation
     |> Annotation.changeset(attrs)
@@ -124,31 +77,10 @@ defmodule Nota.Annotations do
     |> Repo.transaction()
   end
 
-  @doc """
-  Deletes a Annotation.
-
-  ## Examples
-
-      iex> delete_annotation(annotation)
-      {:ok, %Annotation{}}
-
-      iex> delete_annotation(annotation)
-      {:error, %Ecto.Changeset{}}
-
-  """
   def delete_annotation(%Annotation{} = annotation) do
     Repo.delete(annotation)
   end
 
-  @doc """
-  Returns an `%Ecto.Changeset{}` for tracking annotation changes.
-
-  ## Examples
-
-      iex> change_annotation(annotation)
-      %Ecto.Changeset{source: %Annotation{}}
-
-  """
   def change_annotation(%Annotation{} = annotation) do
     Annotation.changeset(annotation, %{})
   end
