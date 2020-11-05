0c419c418cea546d1c8440e855b81c0f4933b29b
Benjamin Leeds <bleeds@samaritan.org>
2020-11-04 21:22:30 -0500

    config

--- config/prod.exs
+++ config/prod.exs
@@ -17,7 +17,7 @@ config :nota, NotaWeb.Endpoint,
   load_from_system_env: true,
   url: [host: System.get_env("HOST"), port: System.get_env("PORT")],
   secret_key_base: System.get_env("SECRET_KEY_BASE"),
-  cache_static_manifest: "priv/static/cache_manifest.json"
+  server: true
 
 config :phoenix, :serve_endpoints, true
 
